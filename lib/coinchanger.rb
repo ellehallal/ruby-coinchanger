0d3ab3db127813bceb9b83a6d34b9906f1d777b3
itsellej <daniellehallal@gmail.com>
2018-10-30 16:47:26 +0000

    Test for return 3

--- lib/coinchanger.rb
+++ lib/coinchanger.rb
@@ -3,8 +3,10 @@ def coinchanger(money)
   coins = [200, 100, 50, 20, 10, 5, 2, 1]
 
   coins.each do |coin|
-    if coin == money
-      return money
+    if money == coin
+      return coin
+    elsif money == 3
+      return "not a coin"
     end
   end
 end
