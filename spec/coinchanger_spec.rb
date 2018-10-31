06a5afc2eb8bea337b7d471662ebb40ae0c20c17
itsellej <daniellehallal@gmail.com>
2018-10-31 14:29:09 +0000

    returns 2 equal coins when given money, if divisible by 2

--- spec/coinchanger_spec.rb
+++ spec/coinchanger_spec.rb
@@ -12,9 +12,12 @@ describe 'coinchanger' do
 
   end
 
-  # it "returns 0 when given 3" do
-  #   expect(coinchanger(3)).to eq 0
-  # end
+  [200, 20, 2].each do |coin|
+    it "returns 2 equal coins when given money, if divisible by 2" do
+      expect(coinchanger(coin * 2)).to eq [coin] * 2
+    end
+  end
+
 
   (201..251).each do |coin|
     it "returns 0 when given a number which is not in the coin array" do
@@ -30,8 +33,5 @@ describe 'coinchanger' do
     expect(coinchanger(40)).to eq [20, 20]
   end
 
-  # it "returns [2, 2, 2] when given 6" do
-  #   expect(coinchanger(6)).to eq [2, 2, 2]
-  # end
 
 end
