a1d38f5d87eb7260d1822d7f75cf8bd6d437c245
itsellej <daniellehallal@gmail.com>
2018-10-31 13:08:49 +0000

    returns 3 equal coins when given money, if divisible by 3

--- lib/coinchanger.rb
+++ lib/coinchanger.rb
@@ -2,14 +2,19 @@ def coinchanger(money)
 
   coins = [200, 100, 50, 20, 10, 5, 2, 1]
   change = 0
+
   half = money / 2
-  remain = money % 2
+  remainhalf = money % 2
+  third = money / 3
+  remainthird = money % 3
 
   coins.each do |coin|
     if money == coin
        return coin
-     elsif remain == 0 && half == coin
+     elsif remainhalf == 0 && half == coin
        change = half, half
+     elsif remainthird == 0 && third == coin
+       change = third, third, third
     end
   end
   change
--- spec/coinchanger_spec.rb
+++ spec/coinchanger_spec.rb
@@ -5,11 +5,12 @@ describe 'coinchanger' do
     it "returns coin when given coin" do
       expect(coinchanger(coin)).to eq coin
     end
+
   end
 
-  it "returns 0 when given 3" do
-    expect(coinchanger(3)).to eq 0
-  end
+  # it "returns 0 when given 3" do
+  #   expect(coinchanger(3)).to eq 0
+  # end
 
   (201..251).each do |coin|
     it "returns 0 when given a number which is not in the coin array" do
@@ -23,6 +24,16 @@ describe 'coinchanger' do
 
   it "returns [20, 20] when given 40" do
     expect(coinchanger(40)).to eq [20, 20]
+  end
+
+  # it "returns [2, 2, 2] when given 6" do
+  #   expect(coinchanger(6)).to eq [2, 2, 2]
+  # end
+
+  [600, 300, 150, 60, 30, 15, 6, 3].each do |money|
+    it "returns 3 equal coins when given money" do
+      expect(coinchanger(money)).to eq [money / 3] * 3
+    end
   end
 
 end
