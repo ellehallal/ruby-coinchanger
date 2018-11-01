7c64b2b69c3f0963d495452a4f2bae9d07b5eb13
itsellej <daniellehallal@gmail.com>
2018-11-01 13:40:01 +0000

    Refactor coinchanger

--- lib/coinchanger.rb
+++ lib/coinchanger.rb
@@ -1,30 +1,12 @@
 def coinchanger(money)
-
   coins = [200, 100, 50, 20, 10, 5, 2, 1]
   change = []
-
   coins.each do |coin|
-    if is_divisible(money, coin)
-      number_of_coins(money, coin).times do
-        change << coin
-        money -= coin
-      end
-    elsif !is_divisible(money, coin)
-      while money >= coin
-          change << coin
-          money -= coin
-      end
+    while money >= coin
+      change << coin
+      money -= coin
     end
   end
-
+  
   change
-end
-
-
-def is_divisible(money, coin)
-  money % coin == 0
-end
-
-def number_of_coins(money, coin)
-  money / coin
 end
